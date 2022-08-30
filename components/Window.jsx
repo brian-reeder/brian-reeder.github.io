ed45f7bcce71bcb2c9dc41dd24d36fcd63541bd0
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-08-30 12:25:01 -0500

    Refactor Window module. Add media queries for dynamic site behavior.

--- components/Window.jsx
+++ components/Window.jsx
@@ -1,20 +1,24 @@
+import styles from '../styles/Window.module.css';
+
 export default function Window({children}) {
 	return (
 	  <>
-	    <div id="window">
-		<div id="toolbar" class="flex">
-		  <ul id="buttons" class="flex">
-		    <li class="button close"></li>
-		    <li class="button"></li>
-		    <li class="button"></li>
-		  </ul>
-		  <p id="window-name"></p>
-		  <div class="buffer"></div>
+		<div class={styles.window}>
+			<div class={styles.header}>
+				<ul class={styles.buttons}>
+					<li class={`${styles.button} ${styles.close}`}></li>
+					<li class={styles.button}></li>
+					<li class={styles.button}></li>
+				</ul>
+				<div class={styles.filler}></div>
+				<div class={styles.title}>Hello, World!</div>
+				<div class={styles.filler}></div>
+				<div class={styles.buttons}></div>
+			</div>
+			<div class={styles.contents}>
+				{ children }
+			</div>
 		</div>
-		<div class="contents">
-		  {children}
-		</div>
-	    </div>
 	  </>
 	);
 }
