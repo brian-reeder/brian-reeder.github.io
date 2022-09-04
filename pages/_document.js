5b5b471b158d6485c4325a3d08313b6f36842f9c
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-09-04 12:05:52 -0500

    Test font swap.

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Default() {
 		    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 		    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 		     <link rel="manifest" href="/site.webmanifest" />
+		     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Red+Hat+Mono&display=swap" />
 		  </Head>
 		  <body>
 		    <Main />
