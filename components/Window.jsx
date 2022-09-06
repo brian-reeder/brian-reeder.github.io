4bf497a10b464cd3d98093a6d0efa4636b999b82
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-09-06 10:10:24 -0500

    Refactor Window buttons.

--- components/Window.jsx
+++ components/Window.jsx
@@ -4,23 +4,29 @@ import styles from '../styles/Window.module.css';
 export default function Window(props) {
 	return (
 		<>
-		  <div className={styles.window}>
-		    <div className={styles.header}>
-		      <ul className={styles.buttons}>
-		        <Link href="/">
-		          <a>
-		            <li className={`${styles.button} ${styles.close}`}></li>
-		          </a>
-		        </Link>
-		        <li className={styles.button}></li>
-		        <li className={styles.button}></li>
+		  <div className={ styles.window }>
+		    <div className={ styles.header }>
+		      <ul className={ styles.buttons }>
+		        <i>
+		          <Link href="/">
+		            <a>
+		              <div className={ `${styles.button} ${styles.close}` }></div>
+		            </a>
+		          </Link>
+		        </i>
+		        <i>
+		          <div className={ styles.button }></div>
+		        </i>
+		        <i>
+		          <div className={ styles.button }></div>
+		        </i>
 		      </ul>
-		      <div className={styles.filler}></div>
-		      <div className={styles.title}>{ props.title }</div>
-		      <div className={styles.filler}></div>
-		      <div className={styles.buttons}></div>
+		      <div className={ styles.filler }></div>
+		      <div className={ styles.title }>{ props.title }</div>
+		      <div className={ styles.filler }></div>
+		      <div className={ styles.buttons }></div>
 		    </div>
-		    <div className={styles.contents}>
+		    <div className={ styles.contents }>
 		      { props.children }
 		    </div>
 		  </div>
