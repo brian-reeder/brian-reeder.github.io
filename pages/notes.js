fffc28140a182fb734276ba8ad5eaede92cda72a
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-09-08 17:21:54 -0500

    Add Document module
    + Add Document module.
    + Add Document css.
    ~ Modify notes page to display a list documents.

--- pages/notes.js
+++ pages/notes.js
@@ -5,13 +5,75 @@ const Window = dynamic(() => import('../components/Window'), {
 	ssr: false,
 });
 
+const Document = dynamic(() => import('../components/Document'), {
+	ssr: false,
+});
+
 export default function Home() {
   return (
     <>
       <Head>
 	<meta name="description" content="Some notes, mind-maps,and other cheetsheets." />
       </Head>
-	  <Window title="Notes"></Window>
+        <Window title="Notes">
+          <ul className="dir">
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+	    </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+	    </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+	    </li>
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+            </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+            </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+            </li>
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+            </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+            </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+            </li>
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+            </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+            </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+            </li>
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+            </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+            </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+            </li>
+            <li>
+              <Document title="test.html" date="Sep 08, 2022" />
+            </li>
+            <li>
+              <Document title="test_longer_even_so_much_longer_more_longer_than_you_can_longer.html" date="Dec 25, 1990" />
+            </li>
+            <li>
+              <Document title="This is a longer blog title.html" date="Feb 14, 2030" />
+            </li>
+
+	  </ul>
+        </Window>
     </>
   )
 }
