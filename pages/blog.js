30d63787db6d514051a6f0bce6b985b7dbd5f123
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-09-10 00:07:05 -0500

    Add Dir module to blog.
    + Add Dir module to blog.
    + Create blog folder to hold blog posts.

--- pages/blog.js
+++ pages/blog.js
@@ -1,17 +1,38 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
+import { getAllPosts } from '../lib/api.ts';
+
 const Window = dynamic(() => import('../components/Window'), {
-	ssr: false,
+        ssr: false,
 });
 
-export default function Home() {
+const Dir = dynamic(() => import('../components/Dir'), {
+        ssr: false,
+});
+
+export default function Home({ allPosts }) {
   return (
     <>
       <Head>
         <meta name="description" content="My personal blog." />
       </Head>
-      <Window title="Blog"></Window>
+      <Window title="Blog">
+        <Dir posts={ allPosts } />
+      </Window>
     </>
   )
 }
+
+export const getStaticProps = async () => {
+        const allPosts = getAllPosts([
+                'title',
+                'date',
+                'slug',
+        ], 'blog');
+
+        return {
+                props: { allPosts },
+        };
+};
+
