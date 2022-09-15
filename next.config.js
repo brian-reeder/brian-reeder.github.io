775e48db3ed78e1bf0c971eba0656fd2280adba9
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2022-09-14 19:00:47 -0500

    Revert "Develop slug template and static paths."
    This reverts commit c3b1d6f4913c48d27fdfdc4b6ed381cb94506718.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-	},
-});
-
-module.exports = withMDX({
+module.exports = {
 	basePath: '',
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-});
+}
